e85f302ae92b661c9d3bc529c1517dcbdad41c49
amita <isrjimbo@gmail.com>
2024-06-04 18:05:58 +0300

    added vehicles scenes and hit logic on path

---
+++ vehicles/tank.gd
@@ -0,0 +1,22 @@
+extends MeshInstance3D
+
+var max_health = 1000
+var current_health = max_health
+
+func _ready():
+	pass
+	
+func _process(delta):
+	pass
+
+func take_damage(damage_amount):
+	current_health -= damage_amount
+	if current_health <= 0:
+		die()
+	print(current_health)
+
+func die():
+	# Implement what happens when the object's health reaches 0
+	print(self.name, " is dead")
+	queue_free()  # This will remove the object from the scene
+
