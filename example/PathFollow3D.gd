851483b6cb3cdbef657db2c4ada2854f46136bd5
orenshm <orenshm@rafael.co.il>
2024-06-06 11:04:02 +0300

    Start convoy moving on aircraft close enough

--- example/PathFollow3D.gd
+++ example/PathFollow3D.gd
@@ -4,8 +4,20 @@ extends PathFollow3D
 @export var set_speed: float = 0.1
 var moving_forward: bool = true
 var is_moving: bool = false
+var start_moving_distance: int = 200
+
+func _aircraft_in_range():
+	var aircraft = get_node_or_null("/root/Example/Aircraft")
+	if (aircraft != null):
+		return aircraft.position.distance_to(self.position) < start_moving_distance
+	return false
 
 func _process(delta):
+	if _aircraft_in_range():
+		start_moving()
+	else:
+		stop_moving()
+	
 	var speed : float
 	if is_moving:
 		speed = set_speed
@@ -27,10 +39,10 @@ func start_moving():
 func stop_moving():
 	is_moving = false
 	
-func _input(event):
-	if event is InputEventKey:
-		if event.pressed and event.unicode == KEY_9:
-			if is_moving:
-				stop_moving()
-			else:
-				start_moving()
+#func _input(event):
+	#if event is InputEventKey:
+		#if event.pressed and event.unicode == KEY_9:
+			#if is_moving:
+				#stop_moving()
+			#else:
+				#start_moving()
