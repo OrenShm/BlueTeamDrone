41619393b4b518e12b36d0bee26e98d25cae312c
d <dolev@rafael.co.il>
2024-06-06 12:49:32 +0300

    win game

---
+++ example/Node3D.gd
@@ -0,0 +1,24 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Timer.connect("timeout", Callable(self, "_check_ambushes"))
+	pass # Replace with function body.
+
+func _check_ambushes():
+	var a = self.get_children()
+	print("launchers found: ", a.size())
+	#.
+	if (!is_instance_valid($AntiAirCannon1)&&
+	!is_instance_valid($AntiAirCannon2)&&
+	!is_instance_valid($AntiAirCannon3)&&
+	!is_instance_valid($AntiAirCannon4)):
+		print("YOU WON!")
+		#_add_song_to_parent()
+		await get_tree().create_timer(2.0).timeout
+		self.queue_free()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
