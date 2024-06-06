01d8c19c1740567611c3b17ddd12de63c73aaecb
d <dolev@rafael.co.il>
2024-06-06 11:25:28 +0300

    stuff for launcher explosion

--- example/scenes/Ambush/AntiAir/AntiAirCannon.gd
+++ example/scenes/Ambush/AntiAir/AntiAirCannon.gd
@@ -1,8 +1,12 @@
 extends Node3D
 
+
+var template_explosion = preload("res://example/scenes/Explosion/Explosion.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Timer.connect("timeout", Callable(self, "_on_launch_timer_elapsed"))
+	self.connect("crashed", Callable(self, "_on_launcher_hit"))
 
 func _on_launch_timer_elapsed():
 	var missile_instance = preload("res://example/scenes/Ambush/AntiAir/AntiAirMissile.tscn").instantiate()
@@ -10,3 +14,13 @@ func _on_launch_timer_elapsed():
 	print("Lunching new missile")
 	self.get_parent().add_child(missile_instance)
 
+func _on_launcher_hit():
+	var new_explosion = template_explosion.instantiate()
+	add_child(new_explosion)
+	new_explosion.global_transform.origin = self.global_transform.origin
+	new_explosion.explode()
+	self.queue_free()
+
+
+func _on_rigid_body_3d_body_entered(body):
+	_on_launcher_hit() # Replace with function body.
