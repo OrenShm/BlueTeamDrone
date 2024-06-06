ced9c907bc2eeef406563c53f5cdda863f164d3d
OrenShm <Orensmo@gmail.com>
2024-06-06 12:18:33 +0300

    Start shoting when aircraft in range (#5)
    Co-authored-by: orenshm <orenshm@rafael.co.il>

--- example/scenes/Ambush/AntiAir/AntiAirCannon.gd
+++ example/scenes/Ambush/AntiAir/AntiAirCannon.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+var aircraft_shoting_range: int = 300
 
 var template_explosion = preload("res://example/scenes/Explosion/Explosion.tscn")
 
@@ -8,11 +9,17 @@ func _ready():
 	$Timer.connect("timeout", Callable(self, "_on_launch_timer_elapsed"))
 	$MeshInstance3D/RigidBody3D.connect("body_shape_entered", Callable(self, "_on_launcher_hit"))
 
+func _aircraft_in_range():
+	var aircraft = get_node_or_null("/root/Example/Aircraft")
+	if (aircraft != null):
+		return aircraft.position.distance_to(self.position) < aircraft_shoting_range
+
 func _on_launch_timer_elapsed():
-	var missile_instance = preload("res://example/scenes/Ambush/AntiAir/AntiAirMissile.tscn").instantiate()
-	missile_instance.position = self.position# + Vector3(0,0,10)
-	print("Lunching new missile")
-	self.get_parent().add_child(missile_instance)
+	if (_aircraft_in_range()):
+		var missile_instance = preload("res://example/scenes/Ambush/AntiAir/AntiAirMissile.tscn").instantiate()
+		missile_instance.position = self.position# + Vector3(0,0,10)
+		print("Lunching new missile")
+		self.get_parent().add_child(missile_instance)
 
 func _on_launcher_hit():
 	var new_explosion = template_explosion.instantiate()
--- example/scenes/Ambush/AntiAir/AntiAirMissile.gd
+++ example/scenes/Ambush/AntiAir/AntiAirMissile.gd
@@ -1,12 +1,19 @@
 extends MeshInstance3D
 
-var muzzle_velocity: float = 5
+var muzzle_velocity: float = 1
 var gravity: Vector3 = Vector3(0, 0, 0)
-#var gravity: Vector3 = Vector3(0, -9.8, 0)
 var velocity: Vector3 = Vector3.ZERO
+var start_time_sec: int = 0
+var retarget_time_sec: int = 1
+var missile_timeout_sec: int = 15
+var explosion_scene = preload("res://example/scenes/Explosion/Explosion.tscn")
+
+func _get_time_sec() -> int:
+	return Time.get_ticks_msec() / 1000.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	start_time_sec = _get_time_sec()
 	var airplaneNode = get_node_or_null("/root/Example/Aircraft")
 	if (airplaneNode != null):
 		shoot_projectile(self.position, airplaneNode.position)
@@ -21,13 +28,29 @@ func shoot_projectile(start_pos: Vector3, destination: Vector3) -> void:
 	velocity = direction * muzzle_velocity
 
 	# Interpolate along the trajectory (lerp from start to destination)
-	var t = 0.5  # Adjust this value (0.0 to 1.0) for desired position
+	var t = 0.001  # Adjust this value (0.0 to 1.0) for desired position
 	transform.origin = lerp(start_pos, destination, t)
 
 # NEVER REMOVE!!!
 func _physics_process(delta: float) -> void:
 	velocity += gravity * delta
 	transform.origin += velocity * delta
+	
+	var time_from_start = _get_time_sec() - start_time_sec
+	if (time_from_start > missile_timeout_sec):
+		print("Self distruction")
+		var explosion = explosion_scene.instantiate()
+		explosion.global_transform.origin = self.global_transform.origin
+		get_parent().add_child(explosion)
+		explosion.explode()
+		self.queue_free()
+		
+	
+	if ((time_from_start) % retarget_time_sec == 0):
+		print("retargeting")
+		var airplaneNode = get_node_or_null("/root/Example/Aircraft")
+		if (airplaneNode != null):
+			shoot_projectile(self.position, airplaneNode.position)
 
 
 # Example usage:
